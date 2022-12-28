38884bab3b18099b3938128ba61424db9a313fdd
juliocesarjcrs <jcrico27@misena.edu.co>
2022-12-28 17:30:52 -0500

    jwt

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import 'express-async-errors';
 import { json } from "body-parser";
-
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
 import { currentUserRouter } from "./routes/current-user";
 import { siginRouter } from "./routes/sigin";
 import { signupRouter } from "./routes/siginup";
 import { siginoutRouter } from "./routes/siginout";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
+
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(siginRouter);
@@ -22,6 +31,16 @@ app.all('*', async ()=>{
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!!!!!!!!");
-});
+const start = async () =>{
+  try {
+    await  mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    console.log('Connected to Mongodb');
+  } catch (err) {
+    console.error(err);
+  }
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+start();
+
--- auth/src/routes/siginup.ts
+++ auth/src/routes/siginup.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response} from "express";
 import { body, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequesError } from "../errors/bad-resquet-error";
 const router = express.Router();
 
 router.post("/api/users/signup", [
@@ -20,10 +22,23 @@ async (req: Request, res: Response) => {
     throw new RequestValidationError(errors.array());
   }
   const { email, password } = req.body;
-  console.log('Creating a user...');
-  throw new DatabaseConnectionError();
 
-  res.send({});
+  const  existingUser = await User.findOne({email});
+  if (existingUser) {
+    throw new BadRequesError("Email in use");
+  }
+ const user = User.build({ email, password});
+ await user.save();
+
+ // GenErate JWT
+ const userJwt = jwt.sign({
+  id: user.id,
+  email: user.email
+ }, 'asf')
+
+ // store it on session object
+  req.session = {jwt: userJwt};
+  res.status(201).send(user);
 });
 
 export {
