24c72f5525e62dda52280c4c7f22c39d3998dae3
juliocesarjcrs <jcrico27@misena.edu.co>
2022-12-29 12:24:43 -0500

    sign in and current user

--- auth/src/routes/sigin.ts
+++ auth/src/routes/sigin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
+import { BadRequesError } from "../errors/bad-resquet-error";
 import { validateRequest } from "../middlewares/validate-request";
+import { User } from "../models/user";
+import { Password } from "../services/password";
+import jwt from "jsonwebtoken";
 const router = express.Router();
 
 router.post(
@@ -14,7 +18,33 @@ router.post(
     ],
     validateRequest,
     async (req: Request, res: Response) => {
-        res.send("Hi there!");
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw new BadRequesError("Invalid credencials 1");
+        }
+        const passwordsMatch = await Password.compare(
+            existingUser.password,
+            password
+        );
+        if (!passwordsMatch) {
+            throw new BadRequesError("Invalid credencials 2");
+        }
+        try {
+          // Generate JWT
+          const userJwt = jwt.sign(
+              {
+                  id: existingUser.id,
+                  email: existingUser.email,
+              },
+              process.env.JWT_KEY!
+          );
+          // store it on session object
+          req.session = { jwt: userJwt };
+          res.status(200).send(existingUser);
+        } catch (err) {
+          console.log(err);
+        }
     }
 );
 
--- auth/src/routes/siginout.ts
+++ auth/src/routes/siginout.ts
@@ -1,11 +1,10 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.post("/api/users/siginout", (req, res) => {
-  res.send("Hi there!");
+router.post("/api/users/signout", (req: Request, res: Response) => {
+    req.session = null;
+    res.send({});
 });
 
-export {
-  router as  siginoutRouter
-};
+export { router as siginoutRouter };
