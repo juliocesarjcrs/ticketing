dafa387089485741522f45b8cd23d3c4c1d7125b
juliocesarjcrs <jcrico27@misena.edu.co>
2022-12-29 11:37:59 -0500

    middleware validate request

--- auth/src/routes/siginup.ts
+++ auth/src/routes/siginup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response} from "express";
-import { body, validationResult } from "express-validator";
+import { body } from "express-validator";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user";
-import { RequestValidationError } from "../errors/request-validation-error";
 import { BadRequesError } from "../errors/bad-resquet-error";
+import { validateRequest } from "../middlewares/validate-request";
 const router = express.Router();
 
 router.post("/api/users/signup", [
@@ -15,12 +15,9 @@ router.post("/api/users/signup", [
   .isLength({ min: 4, max:20})
   .withMessage('Password must be between 4 and 20 characterers'),
 ],
+validateRequest,
 async (req: Request, res: Response) => {
-  const errors = validationResult(req);
 
-  if (!errors.isEmpty()){
-    throw new RequestValidationError(errors.array());
-  }
   const { email, password } = req.body;
 
   const  existingUser = await User.findOne({email});
