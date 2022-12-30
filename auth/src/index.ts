7fede95e994b8c2fa8d937d10075da78d30072e3
juliocesarjcrs <jcrico27@misena.edu.co>
2022-12-30 11:25:17 -0500

    env mongo url in auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,21 +1,21 @@
-
 import mongoose from "mongoose";
 import { app } from "./app";
 
-
-const start = async () =>{
-  if (!process.env.JWT_KEY){
-    throw new Error("JWT_KEY must be defined");
-  }
-  try {
-    await  mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
-    console.log('Connected to Mongodb');
-  } catch (err) {
-    console.error(err);
-  }
-  app.listen(3000, () => {
-    console.log("Listening on port 3000!!!!!!!!");
-  });
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error("JWT_KEY must be defined");
+    }
+    if (!process.env.MONGO_URL) {
+        throw new Error("MONGO_URL must be defined");
+    }
+    try {
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to Mongodb");
+    } catch (err) {
+        console.error(err);
+    }
+    app.listen(3000, () => {
+        console.log("Listening on port 3000!!!!!!!!");
+    });
 };
 start();
-
