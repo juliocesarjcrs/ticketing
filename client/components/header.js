70ca3827c52a2130db68be6ae68a32b15a212d05
juliocesarjcrs <jcrico27@misena.edu.co>
2022-12-29 19:44:17 -0500

    bug link replace a tag

--- client/components/header.js
+++ client/components/header.js
@@ -10,8 +10,8 @@ const Header = ({ currentUser }) => {
     .map(({ label, href }) => {
       return (
         <li key={href} className="nav-item">
-          <Link href={href}>
-            <a className="nav-link">{label}</a>
+          <Link href={href} className="nav-link">
+            {label}
           </Link>
         </li>
       );
@@ -19,8 +19,8 @@ const Header = ({ currentUser }) => {
 
   return (
     <nav className="navbar navbar-light bg-light">
-      <Link href="/">
-        <a className="navbar-brand">GitTix</a>
+      <Link href="/" className="navbar-brand">
+        GitTix
       </Link>
 
       <div className="d-flex justify-content-end">
